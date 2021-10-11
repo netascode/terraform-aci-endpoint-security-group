0e4e5d395eafa6a43f2b1303df1eed91683bb741
danischm <danischm@cisco.com>
2021-10-11 07:15:18 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aci_rest" "fvESg" {
 }
 
 resource "aci_rest" "fvRsScope" {
-  dn         = "${aci_rest.fvESg.id}/rsscope"
+  dn         = "${aci_rest.fvESg.dn}/rsscope"
   class_name = "fvRsScope"
   content = {
     tnFvCtxName = var.vrf
@@ -20,7 +20,7 @@ resource "aci_rest" "fvRsScope" {
 
 resource "aci_rest" "fvRsCons" {
   for_each   = toset(var.contract_consumers)
-  dn         = "${aci_rest.fvESg.id}/rscons-${each.value}"
+  dn         = "${aci_rest.fvESg.dn}/rscons-${each.value}"
   class_name = "fvRsCons"
   content = {
     tnVzBrCPName = each.value
@@ -29,7 +29,7 @@ resource "aci_rest" "fvRsCons" {
 
 resource "aci_rest" "fvRsProv" {
   for_each   = toset(var.contract_providers)
-  dn         = "${aci_rest.fvESg.id}/rsprov-${each.value}"
+  dn         = "${aci_rest.fvESg.dn}/rsprov-${each.value}"
   class_name = "fvRsProv"
   content = {
     tnVzBrCPName = each.value
@@ -38,7 +38,7 @@ resource "aci_rest" "fvRsProv" {
 
 resource "aci_rest" "fvRsSecInherited" {
   for_each   = { for ecm in var.esg_contract_masters : "uni/tn-${ecm.tenant}/ap-${ecm.application_profile}/esg-${ecm.endpoint_security_group}" => ecm }
-  dn         = "${aci_rest.fvESg.id}/rssecInherited-[${each.key}]"
+  dn         = "${aci_rest.fvESg.dn}/rssecInherited-[${each.key}]"
   class_name = "fvRsSecInherited"
   content = {
     tDn = each.key
@@ -47,7 +47,7 @@ resource "aci_rest" "fvRsSecInherited" {
 
 resource "aci_rest" "fvTagSelector" {
   for_each   = { for ts in var.tag_selectors : "${ts.key}-${ts.operator != null ? ts.operator : "equals"}-${ts.value}" => ts }
-  dn         = "${aci_rest.fvESg.id}/tagselectorkey-[${each.value.key}]-value-[${each.value.value}]"
+  dn         = "${aci_rest.fvESg.dn}/tagselectorkey-[${each.value.key}]-value-[${each.value.value}]"
   class_name = "fvTagSelector"
   content = {
     descr         = each.value.description != null ? each.value.description : ""
@@ -59,7 +59,7 @@ resource "aci_rest" "fvTagSelector" {
 
 resource "aci_rest" "fvEPgSelector" {
   for_each   = { for epgs in var.epg_selectors : "uni/tn-${epgs.tenant}/ap-${epgs.application_profile}/epg-${epgs.endpoint_group}" => epgs }
-  dn         = "${aci_rest.fvESg.id}/epgselector-[${each.key}]"
+  dn         = "${aci_rest.fvESg.dn}/epgselector-[${each.key}]"
   class_name = "fvEPgSelector"
   content = {
     descr      = each.value.description != null ? each.value.description : ""
@@ -71,7 +71,7 @@ resource "aci_rest" "fvEPgSelector" {
 
 resource "aci_rest" "fvEPSelector" {
   for_each   = { for iss in var.ip_subnet_selectors : "ip=='${iss.value}'" => iss }
-  dn         = "${aci_rest.fvESg.id}/epselector-[${each.key}]"
+  dn         = "${aci_rest.fvESg.dn}/epselector-[${each.key}]"
   class_name = "fvEPSelector"
   content = {
     descr           = each.value.description != null ? each.value.description : ""
